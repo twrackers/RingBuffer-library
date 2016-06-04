d0508aa6eec7947f5aebaa1e01b0eebf0a1e0dd7
Thomas W Rackers <twrackers@users.noreply.github.com>
2016-06-04 14:49:24 -0400

    Add files via upload

---
+++ sketches/RBTest/RBTest.ino
@@ -0,0 +1,38 @@
+#include <RingBuffer.h>
+
+RingBuffer rb(4);
+
+void show()
+{
+  Serial.println(rb.isEmpty() ? "rb is EMPTY" : "rb is NOT EMPTY");
+  Serial.println(rb.isFull()  ? "rb is FULL"  : "rb is NOT FULL");
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  show();
+  for (byte b = 0; b < 5; ++b) {
+    if (!rb.isFull()) {
+      Serial.print("pushing ");
+      Serial.println(b, DEC);
+      rb.push(b);
+    } else {
+      Serial.print("skipping ");
+      Serial.println(b, DEC);
+    }
+    show();
+  }
+  while (!rb.isEmpty()) {
+    Serial.print("popped ");
+    Serial.println(rb.pop(), DEC);
+    show();
+  }
+  Serial.print("popped ");
+  Serial.println(rb.pop(), DEC);
+  show();
+}
+
+void loop()
+{
+}
